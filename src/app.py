17d791c1ba2a9b3a0c236028c94630de22fb8939
Srinjoy Chakravarty <chakravarty.s@husky.neu.edu>
2020-01-22 10:31:10 +0300

    wrapped db and bcrypt with app :bookmark:

--- src/app.py
+++ src/app.py
@@ -4,19 +4,15 @@ from flask import Flask
 from .config import app_config
 from .models import db, bcrypt
 
-
-
-
-
 def create_app(env_name):
   """
   Create app
   """
-
   # app initiliazation
   app = Flask(__name__)
-
   app.config.from_object(app_config[env_name])
+  bcrypt.init_app(app) # initializing bcrypt
+  db.init_app(app) # add this line
 
   @app.route('/', methods=['GET'])
   def index():
--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -72,8 +72,9 @@ class UserModel(db.Model):
 
 # add this class
 class UserSchema(Schema):
-  id = fields.Int(dump_only = True)
+  id = fields.Str(dump_only = True)
   first_name = fields.Str(required = True)
+  last_name = fields.Str(required = True)
   email_address = fields.Email(required = True)
   password = fields.Str(required = True, load_only = True)
   account_created = fields.DateTime(dump_only = True)
