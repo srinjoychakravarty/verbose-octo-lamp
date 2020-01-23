695a410b35e640488292091730e65cf3f3cd669c
Srinjoy Chakravarty <chakravarty.s@husky.neu.edu>
2020-01-23 04:53:50 +0300

    readying basic authentication

--- src/views/UserView.py
+++ src/views/UserView.py
@@ -5,10 +5,10 @@ from ..models.UserModel import UserModel, UserSchema
 from ..shared.Authentication import Auth
 import re, uuid
 
-from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
-
-auth = HTTPBasicAuth()
+# from flask_httpauth import HTTPBasicAuth
+# from werkzeug.security import generate_password_hash, check_password_hash
+#
+# auth = HTTPBasicAuth()
 
 user_api = Blueprint('user_api', __name__)
 user_schema = UserSchema()
